fd82f6a2d2f92f0cec3ffb9c075e38f8db466704
FolkLoreee <cia.filbert@gmail.com>
2020-01-05 03:17:27 +0700

    Added buttons and logo, fixed sticky

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
 import Slider from "infinite-react-carousel";
-import {Schedule} from "./components/Table"
+import {Schedule} from "./components/Table";
+import "./Dashboard.css";
+import logo from "./images/Logo.png";
+import { MenuButton } from "./components/Buttons";
 
 export class Dashboard extends Component {
   render() {
+
     const settings = {
       adaptiveHeight: false,
       arrows: false,
@@ -15,14 +19,23 @@ export class Dashboard extends Component {
     return (
       <div>
         <div class="sticky">
-          <h1> What The Hack</h1>
-        </div>
+            <div class="inline">
+              <MenuButton></MenuButton> 
+            </div>
+            <div class="inline">
+              <img src={logo} width = {100}/>
+            </div>
+        </div> 
         <Slider {...settings}>
           <div>
+            <h1 class="Heading">Upcoming Schedule</h1>
             <Schedule></Schedule>
           </div>
           <div>
-            <h1>test 2</h1>
+            <h1>Participant Search</h1>
+          </div>
+          <div>
+            <h1>OComm List</h1>
           </div>
         </Slider>
       </div>
